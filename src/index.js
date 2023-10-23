40ba34b4d49ce301e67791782fe81f2b883bc527
SerhiiShymko <shimko456@gmail.com>
2023-10-23 15:54:44 +0300

    вкладеність

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import { App } from './components/App';
+import  {App}  from 'components/App';
+import { GlobalStyle } from 'components/GlobalStyle';
+import { StateExample } from 'components/StateExample/StateExample';
 
 const theme = {
   colors: {
@@ -16,9 +18,10 @@ const theme = {
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      {/* <App /> */}
+      <StateExample />
+      <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>
 );
-
 
